376a18b3ff0411dfe3c3e84cb537fc8c52a265e0
Fressless <50968130+Fressless@users.noreply.github.com>
2021-07-22 19:16:07 +0700

    LOST2.0

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
-$(function(){
+$(function () {
 
-    $('.slider__inner').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-        dots: true,
-        infinite: false,
-        autoplay: true,
+  $('.header__burger').on('click',function(){
+    $('nav ul').slideToggle();
+  })
+
+
+  $(document).ready(function () {
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1200
     });
+  });
+  let header = $('.header');
+  let headerHeight = header.height();
+  let svg = $('svg');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      header.addClass('header__fixed');
+      $('body').css({
+        'paddingTop': headerHeight + 'px'
+      });
+    } else {
+      header.removeClass('header__fixed');
+      $('body').css({
+        'paddingTop': 0
+      })
+    }
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      header.css({
+        'padding': '0 0',
+        'transition': '.3s'
+
+      });
+      svg.css({
+        'background-color': 'red',
+        'transition': '.3s',
+        'width': '200pt',
+        'height': '40pt'
+      })
+    } else {
+      header.css({
+        'padding': '5px 0',
+        'background': '#303841',
+        'transition': '.3s',
+        
+      });
+      svg.css({
+        'background-color': '#fff',
+        'transition': '.3s',
+        'width': '220pt',
+        'height': '44pt',
+
+      })
+    }
+  });
+
+  $('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    dots: true,
+    infinite: false,
+    autoplay: true,
+  });
+
+
 
 });
